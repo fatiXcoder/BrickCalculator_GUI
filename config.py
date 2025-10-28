0ff89fb9d7d91b0b6859cc0c69883e5cdd8e3668
fatiXcoder <engr.fatimabid@gmail.com>
2025-10-28 14:11:16 +0500

    Store credentials and default dimensions

--- config.py
+++ config.py
@@ -1,3 +1,20 @@
-n=4
-if n%2==0:
-    print("Even")
+# config.py
+
+# ---------------- LOGIN CREDENTIALS ----------------
+USERNAME = "admin"
+PASSWORD = "1234"
+
+# ---------------- DEFAULT DIMENSIONS ----------------
+# Wall dimensions in meters
+DEFAULT_WALL = {
+    "length": 5.0,
+    "height": 3.0,
+    "width": 0.3
+}
+
+# Brick dimensions in meters
+DEFAULT_BRICK = {
+    "length": 0.2,
+    "height": 0.1,
+    "width": 0.1
+}
